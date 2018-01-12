6bb4f572ab5ac69e93efc52c161b64b693783d0f
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 20:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function theBeatlesPlay(musician, instrument) {
   var musiciansInsruments = {};
+  for (var i = 0; i < musiciansInsruments.length; i++) {
+    musiciansInsruments.unshift(`${musician} plays ${instrument});
+  }
 }
