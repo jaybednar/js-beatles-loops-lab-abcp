90a3774883c2857494ebe5129d2ad1715ec4c335
Jay Bednar <ironviralmedia@gmail.com>
2018-01-12 20:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var musiciansInstruments = {};
-  for (var i = 0; i < musiciansInstruments.length; i++) {
-    var musiciansInstruments = {`${musicians[i]} play ${instruments[i]}`};
+  for (var i = 0; i < musicians.length && i < instruments.length; i++) {
+    musiciansInstruments{`${musicians[i]} play ${instruments[i]}`};
   }
 }
